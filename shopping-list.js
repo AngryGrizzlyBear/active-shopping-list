e9d662d35eac18d6b8560e148b0ffaca1028e99d
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2024-06-18 10:36:46 -0700

    Shopping List Completion

---
+++ shopping-list.js
@@ -0,0 +1,34 @@
+const myUnorderedList = document.createElement("ul")
+const myInput = document.createElement("input")
+const myButton = document.createElement("button")
+
+// const btn = document.querySelector("#btn");
+
+myButton.addEventListener("click", () => {
+  const myItem = myInput.value;
+  myInput.value = '';
+
+  const myList = document.createElement("li");
+  const mySpan = document.createElement("span");
+  const myListButton = document.createElement("button"); 
+
+
+
+  myList.appendChild(mySpan);
+  mySpan.textContent= myItem;
+  myList.appendChild(myListButton);
+  myListButton.textContent = "Delete";
+  list.appendChild(myList);
+//   myList.appendChild(myListButton);
+//   mySpan.textContent = myItem;
+//   myButton.textContent = "Delete";
+//   myUnorderedList.appendChild(myList);
+
+  myListButton.addEventListener("click", () => {
+    list.removeChild(myList);
+  });
+input.focus();
+});
+
+
+
