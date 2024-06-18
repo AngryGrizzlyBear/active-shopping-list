346b8619232eb039cdda7045c63129ef83c1d43d
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2024-06-18 10:42:12 -0700

    Question Pr

--- shopping-list.js
+++ shopping-list.js
@@ -5,29 +5,30 @@ const myButton = document.createElement("button")
 // const btn = document.querySelector("#btn");
 
 myButton.addEventListener("click", () => {
-  const myItem = myInput.value;
-  myInput.value = '';
+    const myItem = myInput.value;
+    myInput.value = '';
 
-  const myList = document.createElement("li");
-  const mySpan = document.createElement("span");
-  const myListButton = document.createElement("button"); 
+    const myList = document.createElement("li");
+    const mySpan = document.createElement("span");
+    const myListButton = document.createElement("button");
 
 
 
-  myList.appendChild(mySpan);
-  mySpan.textContent= myItem;
-  myList.appendChild(myListButton);
-  myListButton.textContent = "Delete";
-  list.appendChild(myList);
-//   myList.appendChild(myListButton);
-//   mySpan.textContent = myItem;
-//   myButton.textContent = "Delete";
-//   myUnorderedList.appendChild(myList);
+    myList.appendChild(mySpan);
+    mySpan.textContent = myItem;
+    myList.appendChild(myListButton);
+    myListButton.textContent = "Delete";
+    list.appendChild(myList);
 
-  myListButton.addEventListener("click", () => {
-    list.removeChild(myList);
-  });
-input.focus();
+    //   myList.appendChild(myListButton);
+    //   mySpan.textContent = myItem;
+    //   myButton.textContent = "Delete";
+    //   myUnorderedList.appendChild(myList);
+
+    myListButton.addEventListener("click", () => {
+        list.removeChild(myList);
+    });
+    input.focus();
 });
 
 
